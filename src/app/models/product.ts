656eefaf588edb78e9b21fba77200463129cc78f
unknown <ahlem.zheni.97@gmail.com>
2022-09-23 19:49:33 +0200

    Adding required models
    Signed-off-by: unknown <ahlem.zheni.97@gmail.com>

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,7 @@ export class Product {
     id?: number = 0;
     name: string = "";
     price: number = 0;
+    promoPrice?: number = 0;
     image: string = "";
     stars: number = 5;
     quantity?: number = 1;
